a82939c1c4b298718be831243f62792fffbaa91e
VijeghaJegatheeswaran <139634071+VijeghaJegatheeswaran@users.noreply.github.com>
2025-03-16 15:26:49 +0000

    Printing numbers from 1-10

---
+++ second.java
@@ -0,0 +1,24 @@
+//print numbers from 1 to 10
+
+public class second{
+    public static void main(String[] args){
+        for(int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
+
+/*
+@VijeghaJegatheeswaran ➜ /workspaces/E-commerce-Practical-Day01 (main) $ javac second.java
+@VijeghaJegatheeswaran ➜ /workspaces/E-commerce-Practical-Day01 (main) $ java second
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+*/ 
