8cfdb0f421cb8de24a1bb274daba229911745844
VijeghaJegatheeswaran <139634071+VijeghaJegatheeswaran@users.noreply.github.com>
2025-03-16 15:26:21 +0000

    Basic printing a message in java

---
+++ first.java
@@ -0,0 +1,13 @@
+//Print a message
+
+public class first{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
+
+/*
+@VijeghaJegatheeswaran ➜ /workspaces/E-commerce-Practical-Day01 (main) $ javac first.java
+@VijeghaJegatheeswaran ➜ /workspaces/E-commerce-Practical-Day01 (main) $ java first
+Hello World!
+*/ 
