ba9830a520bbb7dcb4f1f3d1c85ceeb24c7965fc
VijeghaJegatheeswaran <139634071+VijeghaJegatheeswaran@users.noreply.github.com>
2025-03-16 15:29:00 +0000

    Printing odd numbers from 1-20

---
+++ odd.java
@@ -0,0 +1,24 @@
+//print the odd numbers between 1 to 20
+
+public class odd{
+    public static void main(String[] args){
+        for(int i=1; i<=20; i=i+2){
+            System.out.println(i);
+        }
+    }
+}
+
+/*
+@VijeghaJegatheeswaran ➜ /workspaces/E-commerce-Practical-Day01 (main) $ javac odd.java
+@VijeghaJegatheeswaran ➜ /workspaces/E-commerce-Practical-Day01 (main) $ java odd
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+*/ 
