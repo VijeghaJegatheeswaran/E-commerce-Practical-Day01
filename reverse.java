41d8ef68723f13e2d3086c43aaa17ea3f3c93ed5
VijeghaJegatheeswaran <139634071+VijeghaJegatheeswaran@users.noreply.github.com>
2025-03-16 15:27:30 +0000

    Printing the numbers in reverse

---
+++ reverse.java
@@ -0,0 +1,24 @@
+//print the numbers in reverse
+
+public class reverse{
+    public static void main(String[] args){
+        for(int i=10; i>=1; i--){
+            System.out.println(i);
+        }
+    }
+}
+
+/*
+@VijeghaJegatheeswaran ➜ /workspaces/E-commerce-Practical-Day01 (main) $ javac reverse.java
+@VijeghaJegatheeswaran ➜ /workspaces/E-commerce-Practical-Day01 (main) $ java reverse
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+*/
